bd4091db6d1c878d983fa32bd3bb5b816f09183b
sharvil-lade <sharvillade9917@gmail.com>
2025-08-27 00:54:44 +0530

    Testing with nginx on EC2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({
     status: 200,
-    message: "hello world testing on EC2",
+    message: "hello world testing on EC2 with nginx as a reverse proxy",
   });
 });
 
